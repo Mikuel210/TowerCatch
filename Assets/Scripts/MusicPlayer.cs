dd2bef1e1f2bb11ccb02f0e82072b965f66360ba
Miguel <loranvallsmiguel@gmail.com>
2025-10-22 15:46:51 +0200

    Added more songs

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,8 +1,16 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class MusicPlayer : MonoBehaviour
 {
+    
     private static MusicPlayer _instance;
+    private AudioSource _audioSource;
+    
+    [SerializeField] private List<AudioClip> songs;
+    private int _songIndex;
     
     void Awake()
     {
@@ -13,5 +21,19 @@ public class MusicPlayer : MonoBehaviour
         
         _instance = this;
         DontDestroyOnLoad(gameObject);
+
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.clip = songs[_songIndex];
+        _audioSource.Play();
     }
+
+    private void Update()
+    {
+        if (_audioSource.isPlaying) return;
+
+        _songIndex = _songIndex == songs.Count - 1 ? 0 : _songIndex + 1;
+        _audioSource.clip = songs[_songIndex];
+        _audioSource.Play();
+    }
+
 }
